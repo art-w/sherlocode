e3db20a2c157dc8def3a711675df9e1f4d0146ad
Arthur Wendling <art.wendling@gmail.com>
2022-02-14 02:10:48 +0100

    www: fix favicon

--- www/www.ml
+++ www/www.ml
@@ -44,7 +44,7 @@ let webserver ~db ~max_age =
                 string_of_tyxml' result))
        ; Dream.get "/s.css" (Dream.from_filesystem "static" "style.css")
        ; Dream.get "/robots.txt" (Dream.from_filesystem "static" "robots.txt")
-       ; Dream.get "/favicon.ico" (Dream.from_filesystem "static" "favicon.svg")
+       ; Dream.get "/favicon.ico" (Dream.from_filesystem "static" "favicon.ico")
        ]
 
 let main path url_tsv max_age =
